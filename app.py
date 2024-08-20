9e7eb0c036626ca66bc78752fdb6faf16996f475
Archie Hickmott <100406766+ArchieHickmott@users.noreply.github.com>
2024-08-20 20:50:40 +1000

    created login system

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
+# stdlib
+from traceback import format_exc, print_exc
+from datetime import datetime
+import datetime as dt
+from random import randint
+
+# lib
+from flask import render_template, redirect, url_for, request, session
+from jinja2 import Environment
+from fortifysql import Database, Table
 from flask import Flask, render_template, request, abort, Response
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
+from flask_bcrypt import Bcrypt
 from wtforms import StringField, DateField, SubmitField, HiddenField
 from wtforms.validators import InputRequired
-from fortifysql import Database, Table
-from random import randint
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-import base64
 
-# Hardcoded RSA private key
-public_key_str = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx+FOnwiQWcF775osIvIW\nscOhUKfyGLi/X7VDVIVv/vqweeiiHRayz+PXoBTlhxnTbkVulKDpX07kZqJJH12g\nGMEdJnRTCFS/X9IXKaMVf1zeTtRvJfc6xskTaVb5PNT/FLiZWp4YSP8XtPhNT/ec\nxso8WqBFH9mOBaFEJwZvbGuf/yhMlpovGlPFl4a61gttMpFHGTalZNgjvC6wAUB4\n/lK4jG2UQbEqFq1vH5PVmBLYeeHIVuWj8UY2wFIRWPGu3QBK90e9AC1cQQj3JB1i\nSkbF1NKlsrpAWOhyrNRG4Ge4wSzmxSg5whB+JYop0knQQUYSbX7sadOxq/kgQ2Yb\n7QIDAQAB\n-----END PUBLIC KEY-----\n'
-
-# Hardcoded RSA public key
-private_key_str = b'-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAx+FOnwiQWcF775osIvIWscOhUKfyGLi/X7VDVIVv/vqweeii\nHRayz+PXoBTlhxnTbkVulKDpX07kZqJJH12gGMEdJnRTCFS/X9IXKaMVf1zeTtRv\nJfc6xskTaVb5PNT/FLiZWp4YSP8XtPhNT/ecxso8WqBFH9mOBaFEJwZvbGuf/yhM\nlpovGlPFl4a61gttMpFHGTalZNgjvC6wAUB4/lK4jG2UQbEqFq1vH5PVmBLYeeHI\nVuWj8UY2wFIRWPGu3QBK90e9AC1cQQj3JB1iSkbF1NKlsrpAWOhyrNRG4Ge4wSzm\nxSg5whB+JYop0knQQUYSbX7sadOxq/kgQ2Yb7QIDAQABAoIBAAiI+/D5jiRvCSpJ\nlJHncYWku2o0ybc5iF5/YIEm84nZTZjfELEaS4dqDMaWrdhvh2JBj7/EAUMjHMiv\nLF0tA82azXJbbEJCUCu4zmlmP+BA6HNISfi+jmF3q58HajwpQj0xxsaiUponXm1J\nXw/HnIe2kg4yotEB2rkt0jmyYiaofGrrUWSUuk8uqRSVAzH0Hz1Znd0rtNIocJkF\ngYNSCW9yfb/K8tBIBHHNLaVWm412RCWm8i61cSSfRg5iTeB74XRUQKvBrsjL2vJF\nK8JOrBKIpGDzUyaboI3W27WIjXABxfFcKiHT+serc6KUi+uMfmbAOG0NqcLR3r8k\nnsWb4JcCgYEA/Tjw2LCMYAOsToqUAdBHLm9vwQrccYbcw+hdHKUKDjr7SY3a4pi6\ndNAKUIUwqKt/HB+x1WS6wfJvI3DlwESY0MAk6kEuKQILsqQEx2XXDfgPQIF3rBWm\nc4BJl07OxRMKea6GMBeqNofiQjmEKX/HuxsS5/NFSGrZ+uV+jQzvRi8CgYEAyhKU\nLrSB/tkXrxqpSWCTJlcb7IfIS/ApgFnqUTEq9X2BT/Uh+99/8xXqBPzitIiEZz1o\nudmMdQow491dBB7X74UfWHo9wPSyT+h5gPoy5sIHUfPVi7WdF9xhDzMAzHFF1Szl\nPT3s0+txZX7fw3shzpj+Ot9kJh/Zgf3VvYALVKMCgYAFmj7p7G4OqcYkLri7mYoZ\noumMEdtyv5Me2oNE4Pnp+rAYnoTbQpnNf9TalzfOY4z6aFEc8Y+YPu7qj9LlgB8J\nb1bhv/NvgNYVa7+XUc+CRZzAxpyJOClooMwABwRYI+W1b4EUi1F+x17gLDmaWXNn\n1l+CtkUwJv0Pqgg0wraAjQKBgQCz1rEWmAORoffYGSxMZ6zOVHw/l+CE5OqqlltZ\ngx6ueNIbLRjfptuipgAuDDpfXZgooZiQvrKofzdftGTSA/k97AYnojNPhPck8ssg\nJIwkns4Q+6czvaBf2fxvoH63dKAnxtsGFBSvzjyPjcjrmqxNyA+KqcHhxDq53ATb\nsBdlbQKBgQDMkAk2GLOG2UGlh7fHa4tIcrtSUNgnu46mZ+zcPowDRrMw7dc5WoLI\nPlT7b8ToylrXHKK30lYTd20sgLIxufDlVpcPln8WAqiHuLctuoEI2JNwhIEEF/3D\n56HIRFap7aqFZlG3VVSVm+JwXYeAPlsWXhNCfu7VPBKGcOjPUQFJCA==\n-----END RSA PRIVATE KEY-----\n'
-
-private_key = load_pem_private_key(private_key_str, password=None)
+# local
+from users import User
+from forms import LoginForm, RegisterForm, UpdateDataForm, ConfirmDelete
+from app_errors import AuthError, AppError
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -26,7 +28,11 @@ def log(request):
     app.logger.info(f"[Database] {request}")
 db.query_logging(True, log)
 db.backup("C:/Users/25hickmar/OneDrive - St Patricks College/Digital Solutions/small projects/quotebook")
-        
+
+crypt = Bcrypt(app)
+User.db = db
+User.crypt = crypt
+
 class QuoteForm(FlaskForm):
     name = StringField("Name", [InputRequired()])
     date = StringField("Date")
@@ -51,7 +57,7 @@ def submit():
     return render_template("submit.html", form=form, quotes=quotes)
 
 @app.route("/")
-def home():
+def index():
     num = db.query("SELECT max(rowid) FROM quotes")[0][0]
     while True:
         id = randint(1, num)
@@ -68,6 +74,150 @@ def quotes():
     quotes: Table = db.quotes
     quotes = quotes()
     return render_template("home.html", quotes=quotes)
+
+# MARK: Login Page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if "user" in session:
+        return redirect(url_for('home'))
+    form: LoginForm = LoginForm()
+    if form.validate_on_submit() and request.method == "POST":
+        # get relevent user data
+        email = form.email.data
+        password = form.password.data
+        id = db.users.get(db.users.userid).filter(email=email).first()
+        
+        # log in algorithm
+        try:
+            id = id[0]
+            user = User(id, password)
+        except TypeError: form.email.errors.append("email does not exist")
+        except AuthError as e:
+            app.logger.warning(f"failed loggin for {id}")
+            db.log_failed_logins.append(userid=id, 
+                                        ip=request.remote_addr, 
+                                        time=datetime.now(), 
+                                        error_message=str(e))
+            form.password.errors.append(e)
+        else: # SUCCESSFUL LOGIN
+            db.log_logins.append(userid=id, ip=request.remote_addr, time=datetime.now())
+            session["user"] = vars(user)
+            return redirect(url_for('home'))
+    return render_template("login.html", form=form)
+
+# MARK: Register Page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if "user" in session:
+        return redirect(url_for('home'))
+    form: RegisterForm = RegisterForm()
+    if form.validate_on_submit() and request.method == "POST":
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data 
+        password = form.password.data
+        
+        if db.users.get("email").filter(email=email).first():
+            form.email.errors.append(ValueError("email is already in use"))
+            return render_template("register.html", form=form)
+        
+        User.create_user(db, first_name, last_name, email, password)
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
+# MARK: Home Page
+# home page (logged in users only)
+@app.route('/home')
+def home():
+    num = db.query("SELECT max(rowid) FROM quotes")[0][0]
+    while True:
+        id = randint(1, num)
+        try:
+            quote = db.query(f"SELECT * FROM quotes WHERE rowid = {id}")[0]
+        except IndexError:
+            pass
+        else:
+            break
+    if not "user" in session:
+        return redirect(url_for("login")), 401
+    user = User.load(**session["user"])
+    if not user.is_logged_in():
+        return redirect(url_for("login"))
+    return render_template("userpage.html", user=user, quote=quote)
+
+# MARK: logout/delete
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        session.pop("user")
+    return redirect(url_for("login"))
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete_account():
+    form = ConfirmDelete()
+    if not "user" in session:
+        return redirect(url_for("index"))
+    user = User.load(**session["user"])
+    if not user.is_logged_in():
+        return redirect(url_for("index"))
+    if not form.validate_on_submit():
+        return render_template("confirm_delete.html", form=form)
+    id = user.id()
+    session.pop("user")
+    db.log_detail_changes.remove(userid=id)
+    db.log_failed_logins.remove(userid=id)
+    db.log_logins.remove(userid=id)
+    db.passwords.remove(userid=id)
+    db.users.remove(userid=id)
+    return redirect(url_for("login"))
+
+# MARK: Admin
+@app.route('/admin')
+def admin():
+    # office_open = dt.time(9, 0)
+    # office_close = dt.time(17, 0)
+    # current_time = datetime.now().time()
+    # if not office_open <= current_time <= office_close:
+        # app.logger.warning("attempted access of admin portal outside of office hours")
+        # return "Admin portal can only accessed during office hours"
+    if not "user" in session:
+        return redirect(url_for('login'))
+    users = db.users
+    user = User.load(**session["user"])
+    permission_level = users.get("PLEVEL").filter(userid=user.id()).first()[0]
+    if (not isinstance(permission_level, int)) or permission_level < 0:
+        app.logger.critical("There exists a user with an invalid permission level")
+        raise AppError("Invalid permission level")
+    requested_user_data = request.args.get("user")
+    if requested_user_data and permission_level != 0:
+        id = requested_user_data
+        return render_template("user_data_lookup.html", 
+                                id=requested_user_data,
+                                first_name=users.get(users.first_name).filter(userid=id).first()[0],
+                                last_name=users.get(users.last_name).filter(userid=id).first()[0],
+                                email=users.get(users.email).filter(userid=id).first()[0],
+                                date_created=users.get(users.date_created).filter(userid=id).first()[0],
+                                plevel=users.get(users.PLEVEL).filter(userid=id).first()[0],
+                                logins=db.log_logins.get("ip", "time").filter(userid=id).order("time DESC").all(),
+                                fails=db.log_failed_logins.get("ip", "time").filter(userid=id).order("time DESC").all(),
+                                changes=db.log_detail_changes.get("detail_changed", "ip", "time").filter(userid=id).order("time DESC").all())
+    if not user.is_logged_in():
+        return redirect(url_for("login")) 
+    if permission_level != 0:
+        users_table = users.get().all()
+        fails = db.query(f"""SELECT fails.userid, users.email, fails.ip, fails.time
+                            FROM log_failed_logins as fails, users WHERE fails.userid = users.userid
+                            LIMIT 5""")
+        logins = db.query(f"""SELECT fails.userid, users.email, fails.ip, fails.time
+                            FROM log_logins as fails, users WHERE fails.userid = users.userid
+                            LIMIT 5""")
+        return render_template('admin.html', 
+                                user=user, 
+                                users=users_table, 
+                                fails=fails, 
+                                logins=logins,
+                                plevel = permission_level)
+    return "NOT ADMIN", 401
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
