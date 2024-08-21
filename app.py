1ee516b81a1dedd3ad75fe3938a05b0815107f3e
Archie Hickmott <100406766+ArchieHickmott@users.noreply.github.com>
2024-08-21 12:21:16 +1000

    improved compatability

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import datetime as dt
 from random import randint
 import json
+import os
 
 # lib
 from flask import render_template, redirect, url_for, request, session
@@ -25,7 +26,46 @@ app = Flask(__name__)
 app.config["PERMANENT_SESSION_LIFETIME"] = dt.timedelta(hours=1)
 bootstrap = Bootstrap5(app)
 app.config["SECRET_KEY"] = "attendance is the biggest indicator for success"
-db = Database("quote.db")
+if os.path.isfile("quote.db"):
+    db = Database("quote.db")
+else:
+    with open("quote.db", "w") as file:
+        pass
+    db = Database("quote.db")
+    db.multi_query("""
+CREATE TABLE log_failed_logins (
+    userid        TEXT REFERENCES users (userid),
+    ip            TEXT,
+    time          TEXT,
+    error_message TEXT
+);
+CREATE TABLE log_logins (
+    userid TEXT REFERENCES users (userid),
+    ip     TEXT,
+    time   TEXT
+);
+CREATE TABLE passwords (
+    userid TEXT REFERENCES users (userid) 
+                NOT NULL
+                UNIQUE,
+    hash   BLOB
+);
+CREATE TABLE quotes (
+    name     TEXT,
+    year     TEXT,
+    quote    TEXT,
+    likes    TEXT    DEFAULT "[]",
+    numlikes INTEGER DEFAULT (0) 
+);
+CREATE TABLE users (
+    userid       TEXT    PRIMARY KEY,
+    first_name   TEXT    NOT NULL,
+    last_name    TEXT,
+    email        TEXT    NOT NULL
+                         UNIQUE,
+    date_created TEXT,
+    PLEVEL       INTEGER DEFAULT (0) 
+);""")
 def log(request: str):
     if request.strip() == "COMMIT" or request.strip() == "BEGIN":
         return
