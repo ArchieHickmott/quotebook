e5bd1dfe0884fcce7375817b1dde8574e22e95d4
Nyx <79072303+Helios-fr@users.noreply.github.com>
2024-08-22 17:27:12 +1000

    Moved the database creation code to its own file

--- app.py
+++ app.py
@@ -38,41 +38,7 @@ else:
     with open("quote.db", "w") as file:
         pass
     db = Database("quote.db")
-    db.multi_query("""
-CREATE TABLE users (
-    userid       TEXT    PRIMARY KEY,
-    first_name   TEXT    NOT NULL,
-    last_name    TEXT,
-    email        TEXT    NOT NULL
-                         UNIQUE,
-    date_created TEXT,
-    PLEVEL       INTEGER DEFAULT (0) 
-);
-CREATE TABLE log_failed_logins (
-    userid        TEXT REFERENCES users (userid),
-    ip            TEXT,
-    time          TEXT,
-    error_message TEXT
-);
-CREATE TABLE log_logins (
-    userid TEXT REFERENCES users (userid),
-    ip     TEXT,
-    time   TEXT
-);
-CREATE TABLE passwords (
-    userid TEXT REFERENCES users (userid) 
-                NOT NULL
-                UNIQUE,
-    hash   BLOB
-);
-CREATE TABLE quotes (
-    name     TEXT,
-    year     TEXT,
-    quote    TEXT,
-    likes    TEXT    DEFAULT "[]",
-    numlikes INTEGER DEFAULT (0) 
-);
-""")
+    db.multi_query(open("quote.sql").read())
     db.reload_tables()
 def log(request: str):
     if request.strip() == "COMMIT" or request.strip() == "BEGIN":
