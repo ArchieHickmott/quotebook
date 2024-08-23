c13e1e5674cc2f8004de40f08c2ff76454c6f28c
Helios <email@example.com>
2024-08-23 14:24:01 +1000

    created initial version of usermanager

--- utils/userManager.py
+++ utils/userManager.py
@@ -8,6 +8,8 @@ likes(user_id, quote_id)
 comments(user_id, quote_id, comment)
 '''
 
+from time import time
+
 if __name__ == '__main__':
     from utils import DatabaseManager
 
@@ -30,7 +32,28 @@ class UserManager:
         :param password_hash: The password hash of the user.
         '''
         
-        self.dbm.query('INSERT INTO users (name, email, password_hash, created_at, plevel) VALUES (?, ?, ?, ?, ?)', (name, email, password_hash, '0', 1))
+        self.dbm.query('INSERT INTO users (name, email, password_hash, created_at, plevel) VALUES (?, ?, ?, ?, ?)', (name, email, password_hash, int(time()), 1))
+
+    def update_user(self, user_id: int, name: str=None, email: str=None, password_hash: str=None, plevel: int=None):
+        '''
+        Update a user in the database.
+        :param user_id: The ID of the user.
+        :param name: The name of the user.
+        :param email: The email of the user.
+        :param password_hash: The password hash of the user.
+        '''
+        for item in [name, email, password_hash, plevel]:
+            if item is None:
+                item = self.get_user(user_id)[item]
+        
+        self.dbm.query('UPDATE users SET name = ?, email = ?, password_hash = ? WHERE id = ?', (name, email, password_hash, user_id))
+
+    def delete_user(self, user_id: int):
+        '''
+        Delete a user from the database.
+        :param user_id: The ID of the user.
+        '''
+        self.dbm.query('DELETE FROM users WHERE id = ?', (user_id,))
     
 
     
