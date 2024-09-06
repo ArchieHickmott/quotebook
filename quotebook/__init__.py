6eefd679bf7025a9ed4f1bede2269a3cd127ae7a
Archie Hickmott <100406766+ArchieHickmott@users.noreply.github.com>
2024-09-06 16:47:26 +1000

    stopped the forced use of load_config()

--- quotebook/__init__.py
+++ quotebook/__init__.py
@@ -1,10 +1,14 @@
+from typing import Tuple
+from flask_socketio import SocketIO
+from flask import Flask
 from quotebook.app import App
 from quotebook.config import load_config
 
-def main(host:str=None, port:int=None, debug:bool|None=None, config_path:str=""):
-    App.config = load_config(path=config_path)
-    host = host or App.config["host"]
-    port = port or App.config["port"]
-    debug = debug or App.config["debug"]
+def create_app(host:str=None, port:int=None, debug:bool|None=None, config_path:str="") -> Tuple[SocketIO, Flask]:
+    if config_path:
+        App.config = load_config(path=config_path)
+        host = host or App.config["host"]
+        port = port or App.config["port"]
+        debug = debug or App.config["debug"]
     socket, flask_app = App()
-    socket.run(flask_app, host, port, debug=debug)
+    return socket, flask_app
