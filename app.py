68bf50faf24b1fd63ef34382e10276e4d091a8fd
Helios <79072303+Helios-fr@users.noreply.github.com>
2024-10-29 10:24:54 +1000

    Deleted all files

--- app.py
+++
@@ -1,9 +0,0 @@
-from quotebook import create_app 
-
-socketio, app = create_app(config_path="config.toml")
-socketio.run(app, 
-             "0.0.0.0", 
-             5000,
-             use_reloader=True,
-             debug=False,
-             allow_unsafe_werkzeug=True)
--- quotebook.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-@REM uncomment to auto-install packages
-@REM pip install -r requirements.txt
-echo HI
-python -m quotebook --host 0.0.0.0 --port 5000
