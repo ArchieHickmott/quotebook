e861566f3c502629fee2f78378d809423c8c1b05
Helios <79072303+Helios-fr@users.noreply.github.com>
2024-10-29 10:20:25 +1000

    Delete quotebook directory

--- quotebook/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .databaseManager import db
-from .crypt import check_password_hash, generate_password_hash
-from .logger import logger
-from .quoteManager import qm
-from .userManager import User, um, login_required
